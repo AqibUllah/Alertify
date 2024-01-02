473275d65ca8caf0ee292def779a48c8816668a9
Aqib Lodhi <appleone1@Apples-MacBook-Pro-2.local>
2024-01-02 21:41:43 +0500

    fixed design issue

--- src/resources/views/components/success.blade.php
+++ src/resources/views/components/success.blade.php
@@ -1,9 +1,9 @@
 <div class="alert alert-success {{ config('alertify.animation') }} ">
-    @if(config('alertify.pulse'))
-    <div class="pulse"></div>
-    @endif
     <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
     <div class="left">
+        @if(config('alertify.pulse'))
+            <div class="pulse"></div>
+        @endif
         @if(config('alertify.icons'))
             <img src="{{ asset('vendor/alertify/assets/icons/success.svg') }}" alt="">
         @endif
